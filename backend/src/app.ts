7192b8bedd1bbe665ed5e98fcc922ef2a2ceb5ad
febollinger <feernanda.bollinger@gmail.com>
2025-11-02 21:12:05 -0300

    feat: adding route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import { analyticsRouter } from "./routers/analytics.router.js";
 
 export const app = express()
 app.use(express.json())
+
+analyticsRouter.use("/home", analyticsRouter)
