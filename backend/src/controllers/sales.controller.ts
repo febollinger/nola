f2b2689443aeeb9260da2334a3fbeb9c486d8367
febollinger <feernanda.bollinger@gmail.com>
2025-11-03 22:20:33 -0300

    feat: adding sales filters

--- backend/src/controllers/sales.controller.ts
+++ backend/src/controllers/sales.controller.ts
@@ -1,3 +1,21 @@
-export const getSalesController= () => {
-    
-}
+import type { Request, Response } from "express";
+import { getFilteredSalesService } from "../services/sales.service.js";
+
+export const getSalesController = async (req: Request, res: Response) => {
+  try {
+    const { productId, channel, startDate, endDate, skip, take } = req.query;
+
+    const salesFiltered = await getFilteredSalesService({
+      productId: productId ? Number(productId) : undefined,
+      channel: channel as string | undefined,
+      startDate: startDate as string | undefined,
+      endDate: endDate as string | undefined,
+      skip: skip ? Number(skip) : undefined,
+      take: take ? Number(take) : undefined,
+    });
+
+    res.json(salesFiltered);
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- backend/src/services/sales.service.ts
+++ backend/src/services/sales.service.ts
@@ -1,3 +1,33 @@
-export const getSalesService = () => {
+import { Repository } from "typeorm";
+import { AppDataSource } from "../data-source.js"
+import { Sales } from "../entities/Sales.js";
+
+export const getFilteredSalesService = async (filters: any) => {
+  const salesRepository: Repository<Sales> = AppDataSource.getRepository(Sales);
+
+  const getSales = salesRepository.createQueryBuilder("sales")
+    .leftJoinAndSelect("sales.productSales", "product");
+
+  if (filters.productId) getSales.andWhere("products.id = :productId", { productId: filters.productId });
+  if (filters.channel) getSales.andWhere("sales.channel = :channel", { channel: filters.channel });
+  if (filters.startDate) getSales.andWhere("sales.createdAt >= :startDate", { startDate: filters.startDate });
+  if (filters.endDate) getSales.andWhere("sales.createdAt <= :endDate", { endDate: filters.endDate });
+
+  getSales.orderBy("sales.createdAt", "DESC");
+
+
+  const page = filters.page && filters.page > 0 ? filters.page : 1;
+  const limit = filters.limit && filters.limit > 0 ? Math.min(filters.limit, 100) : 10;
+  const skip = (page - 1) * limit;
+
+  getSales.skip(skip).take(limit);
+
+  const [data, total] = await getSales.getManyAndCount();
     
-}
+  return {
+    data,
+    total,
+    page,
+    lastPage: Math.ceil(total / limit),
+  };
+};
